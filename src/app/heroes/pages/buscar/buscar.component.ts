015eca60b1ab00697ccb73e9ae0679d7c0e950b4
SammyCaro <sammy.caro94@gmail.com>
2022-09-26 15:09:38 -0300

    -.Se crea modulo auth con las paginas login y register

---
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buscar',
+  templateUrl: './buscar.component.html',
+})
+export class BuscarComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
