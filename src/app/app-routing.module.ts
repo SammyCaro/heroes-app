338b4aec3df9ce5c96abf6fecf3d971597299460
SammyCaro <sammy.caro94@gmail.com>
2022-09-26 15:27:10 -0300

    Se agregan las rutas principales, pero solo es la del 404. Se hace así ya que como se va a implementar el lazyload no es necesario especificar las rutas en duro en el appRoutingModule, sino que se va a especificar en cada uno de los componentes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+/* componentes */
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
+
+const routes: Routes = [
+  {
+    path: '404',
+    component: ErrorPageComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '404',
+    //component: ErrorPageComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
