55c07991982abcb4aeb9e438b48720352721df6f
SammyCaro <sammy.caro94@gmail.com>
2022-09-27 13:00:13 -0300

    Se agrega servicio para la manipulacion de la data de heroes. El servicio esta en el modulo de heroes pero el modulo HttpClientModule se encuenta de manera global en el app.module. FALTA AGREGAR EL TIPADO PARA HACER LOS REQUEST

--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
 })
 export class ListadoComponent implements OnInit {
-  constructor() {}
+  constructor(private heroesService: HeroesService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.heroesService.getHeroes().subscribe((heroes) => {
+      console.log(heroes);
+    });
+  }
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HeroesService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getHeroes() {
+    return this.http.get('http://localhost:3000/heroes');
+  }
 }
