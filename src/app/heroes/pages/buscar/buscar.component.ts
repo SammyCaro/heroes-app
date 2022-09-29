6b927252f8dce8270fd3899a2dafd496391d154e
SammyCaro <sammy.caro94@gmail.com>
2022-09-29 14:09:33 -0300

    Se añaden sugerencias en el input buscar del componente buscar. Se agregan validaciones como si al buscar no llega un array vacio y cuando no hay resultados no reviente la app

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -10,25 +10,39 @@ import { HeroesService } from '../../services/heroes.service';
 export class BuscarComponent implements OnInit {
   termino: string = '';
   heroes: Heroes[] = [];
-  heroeSeleccionado!: Heroes;
+  heroeSeleccionado: Heroes | undefined;
+  sinHeroe!: boolean;
 
   constructor(private heroesService: HeroesService) {}
 
   ngOnInit(): void {}
 
   buscando() {
-    this.heroesService.getSugerencias(this.termino).subscribe((heroes) => {
-      this.heroes = heroes;
-    });
+    this.heroesService
+      .getSugerencias(this.termino.trim())
+      .subscribe((heroes) => {
+        this.heroes = heroes;
+        if (heroes.length > 0 && this.termino != '') {
+          this.sinHeroe = false;
+        } else {
+          this.sinHeroe = true;
+        }
+        //devuelve array vacio si el termino es cualquier cosa
+        console.log(this.heroes);
+      });
   }
 
   opcionSeleccionada(event: MatAutocompleteSelectedEvent) {
+    if (!event.option.value) {
+      //si lo que llega es undefined se hace el return para que no lance error
+      this.heroeSeleccionado = undefined;
+      return;
+    }
     const heroe: Heroes = event.option.value;
     this.termino = heroe.superhero;
-
+    console.log(heroe);
     this.heroesService.getHeroesById(heroe.id!).subscribe((heroes) => {
       this.heroeSeleccionado = heroes;
-      console.log(this.heroeSeleccionado);
     });
   }
 }
